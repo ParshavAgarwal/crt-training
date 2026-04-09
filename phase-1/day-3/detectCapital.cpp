710a12d31e16f6d99430b84b25f46d415eb67639
Parshav Agarwal <parshavagarwal.it27@jecrc.ac.in>
2026-04-09 23:22:10 +0530

    Complete Detect Capital challenge in day-3

---
+++ phase-1/day-3/detectCapital.cpp
@@ -0,0 +1,70 @@
+/*Return True if:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool detectCapitalUse(string word) {
+        
+    int upper = 0;
+    int lower = 0;
+    bool first = isupper(word[0]);
+
+    for(auto x:word){
+        if(isupper(x)) upper++;
+        if(islower(x)) lower++;
+    }
+
+    if(upper == word.size() || lower == word.size()){
+        return true;
+    }
+    else if(first && upper == 1){
+        return true;
+    }
+
+    return false;
+
+    /*
+    //for all Upper
+    bool allUpper = true;
+    for(int i=0; i<word.size(); i++){
+        if (!isupper(word[i])){
+            allUpper = false;
+        }
+    }
+    if (allUpper) return true;
+
+    //for all lower
+    bool allLower = true;
+    for(int i=0; i<word.size(); i++){
+        if(!islower(word[i]))
+            allLower = false;
+    }
+    if (allLower) return true;
+
+    // for capital
+    bool cap = true;
+    if (isupper(word[0])){
+        for(int i=1; i<word.size(); i++){
+            if (!islower(word[i]))
+                cap = false;
+        }
+        if (cap) return true;
+    }
+
+    return false;*/
+}
+
+int main(){
+    string word;
+    cout<< "Enter a word: ";
+    cin >> word;
+
+    cout << detectCapitalUse(word)<< endl;
+
+    return 0;
+}
